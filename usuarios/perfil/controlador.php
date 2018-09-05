29bfa3b1a0be5c5086b312347ba128dc39c770d8
Maria Teresa Ortube <mariateresaortube@gmail.com>
2018-09-05 15:06:28 +0200

    Actualizar datos del perfil con el email y la contraseña

--- usuarios/perfil/controlador.php
+++ usuarios/perfil/controlador.php
@@ -1,18 +1,34 @@
+<?php require_once(dirname(__FILE__)."/../conexion.php"); ?>
+
 <?php
 
-function cambiaEmail($email, $email2){
+function cambiaEmail($email){
     $conexion = getConexion();
-    $consulta = "SELECT * FROM usuarios WHERE usuario='usuarioSesion';";
+    $usuarioSesion = $_SESSION["usuario"];
+    $consulta = "UPDATE usuarios SET email='$email' WHERE usuario='$usuarioSesion';";
     $resultado = mysqli_query($conexion,$consulta) or die("Consulta errónea");
 
     if($resultado){
-        
+        return true;
+    }else{
+        return false;
     }
     
-
 }
+
+
 function cambiaContrasena($contrasena){
     $conexion = getConexion();
+    $usuarioSesion = $_SESSION["usuario"];
+    $passHash = password_hash($contrasena, PASSWORD_BCRYPT); //Función para cifrar contraseña.
+    $consulta = "UPDATE usuarios SET contrasena='$passHash' WHERE usuario='$usuarioSesion';";
+    $resultado = mysqli_query($conexion,$consulta) or die("Consulta errónea");
+
+    if($resultado){
+        return true;
+    }else{
+        return false;
+    }
 }
 
 $usuarioSesion = $_SESSION["usuario"];
@@ -25,21 +41,26 @@ if (isset($usuarioSesion)) {
       $email2 = $_POST["email2"];
       $contrasena = $_POST["contrasena"];
       $contrasena2 = $_POST["contrasena2"];
+      
 
-      if($email AND $email2){
-          cambiaEmail($email, $email2);
-      }else{
-          addError("El email no coincide");
+      if($email AND $email != ""){
+          if($email == $email2){
+              cambiaEmail($email);
+              addMensaje("Datos actualizaos");
+          }else{
+              addError("Error en los datos");
+          }  
+      }if($contrasena AND $contrasena != ""){
+          if($contrasena == $contrasena2){
+              cambiaContrasena($contrasena);
+              addMensaje("Datos actualizaos");
+          }else{
+              addError("Error en los datos");
+          }
       }
 
-      if($contrasena AND $contrasena2){
-          cambiaContrasena($contrasena, $contrasena2);
-      }else{
-          addError("La contraseña no coincide,verifique");
-      }
-
-   }
-}
+    }  
+  }
 
 
 
