7a5ddcb5ff63ffbb3cfa415682f918c2d63c260c
Maria Teresa Ortube <mariateresaortube@gmail.com>
2018-10-29 19:50:38 +0100

    WIP productos

--- prod/listaproductos/controlador.php
+++ prod/listaproductos/controlador.php
@@ -12,7 +12,7 @@ function getProductos(){
 
     $conexion = getConexion();
     $consulta = "SELECT * FROM productos";
-    $resultado = mysqli_query($conexion,$consulta) or die("Consulta errónea");
+    $resultado = mysqli_query($conexion,$consulta) or die("Consulta errónea linea15");
 
     $productos = array();
 
@@ -24,47 +24,42 @@ function getProductos(){
     return $productos;
  }
  mysqli_free_result($resultado);
-}
-    
-function enlaceImagen(){
-     
-    $conexion = getCone
-}
+}    
+
 
 function getImageMarke($producto_id){
 
     $conexion = getConexion();
-    $consulta = "SELECT * FROM archivo_productos WHERE WHERE tipo = 'marketing' AND producto_id = '$producto_id';";
-    $resultado = mysqli_query($conexion,$consulta) or die("Consulta errónea");
+    $consulta = "SELECT * FROM archivo_productos  WHERE tipo = 'marketing' AND productos_id = '$producto_id';";
+    $resultado = mysqli_query($conexion,$consulta) or die(mysqli_error($conexion));
 
     $foto = array();
 
     if ($resultado){
         while ($row = mysqli_fetch_assoc($resultado)) {
-            array_push($foto, base64_encode($row['contenido_archivo']));
+
+            return base64_encode($row['contenido_archivo']);
     }
 
-    return $foto;
- }
- mysqli_free_result($resultado);
+    }
 }
 
-function getImageReal(){
+function getImageReal($producto_id){
 
-    $conexion = getConexion($producto_id);
-    $consulta = "SELECT * FROM archivo_productos WHERE WHERE tipo = 'real' AND producto_id = '$producto_id';";
-    $resultado = mysqli_query($conexion,$consulta) or die("Consulta errónea");
+    $conexion = getConexion();
+    $consulta = "SELECT * FROM archivo_productos WHERE tipo = 'real' AND productos_id = '$producto_id';";
+    $resultado = mysqli_query($conexion, $consulta) or die( mysqli_error($conexion));
 
     $foto = array();
 
     if ($resultado){
         while ($row = mysqli_fetch_assoc($resultado)) {
-            array_push($foto, base64_encode($row['contenido_archivo']));
+           return base64_encode($row['contenido_archivo']);
     }
+    
 
-    return $foto;
  }
- mysqli_free_result($resultado);
+
 }
     
 
